292e305aa90caa3f4131a74d0577c4ba774a5cab
Vedang Mishrra <145650247+justvedang@users.noreply.github.com>
2026-03-03 15:00:01 +0530

    created project page layout

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,5 +1,6 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { verifyAuth } from "./auth";
 
 export const create=mutation({
     args:{
@@ -8,26 +9,40 @@ export const create=mutation({
     },
 
     handler:async(ctx,args)=>{
-        const identity=await ctx.auth.getUserIdentity();
-        if(!identity){
-            throw new Error("Unauthorized");
-        }
-        await ctx.db.insert("projects",{
+        const identity=await verifyAuth(ctx);
+        const projectId= await ctx.db.insert("projects",{
             name:args.name,
-            ownerId:identity?.subject,
+            ownerId:identity.subject,
+            updatedAt:Date.now(),
         });
+
+        return projectId;
     },
 });
 
+export const getPartial=query({
+    args:{
+        limit:v.number(),
+
+    },
+    handler:async(ctx,args)=>{
+
+        const identity=await verifyAuth(ctx);
+
+        return await ctx.db.query("projects")
+        .withIndex("by_owner",(q)=>q.eq("ownerId",identity.subject))
+        .take(args.limit);
+
+    },
+})
+
 export const get=query({
-    args:{},
+    args:{
+
+    },
     handler:async(ctx)=>{
 
-        const identity=await ctx.auth.getUserIdentity();
-
-        if(!identity){
-            return [];
-        }
+        const identity=await verifyAuth(ctx);
 
         return await ctx.db.query("projects")
         .withIndex("by_owner",(q)=>q.eq("ownerId",identity.subject))
