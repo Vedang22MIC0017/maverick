4480a80f2c7143c1573a46b255af983714ba0858
Vedang Mishrra <145650247+justvedang@users.noreply.github.com>
2026-03-03 11:35:06 +0530

    added scrapping by firecrawl

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,16 +1,47 @@
 import { generateText } from "ai";
 import { inngest } from "./client";
 import { google } from "@ai-sdk/google";
+import { firecrawl } from "@/lib/firecrawl";
+
+const URL_REGEX=/https?:\/\/[^\s]+/g;
 
 export const demoGenerate = inngest.createFunction(
   { id: "demo-generate" },
   { event: "demo/generate" },
-  async ({ step }) => {
+  async ({ event,step }) => {
+    const {prompt}=event.data as {prompt:string};
+    const urls=await step.run("extract-urls",async()=>{
+      return prompt.match(URL_REGEX) ?? [];
+    }) as string[];
+
+    //extended knowledge of model using firecral by 
+
+    const scrappedContent=await step.run("scrape-urls",async()=>{
+      const results=await Promise.all(
+        urls.map(async (url)=>{
+          const result=await firecrawl.scrapeUrl(url,{
+            formats:["markdown"]
+          },);
+
+          if (!result.success) {
+          console.error("Scrape failed:", result.error);
+          return null;
+        }
+          return result.markdown ?? null ;
+
+        })
+      )
+      return results.filter(Boolean).join("\n\n");
+    });
+
+    const finalPrompt=scrappedContent ? `Context:\n ${scrappedContent}\n\nQuestion: ${prompt}`:prompt;
     await step.run("generate text",async() =>{
         return await generateText({
             model: google('gemini-2.5-flash'),
-            prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+            prompt: finalPrompt,
         });
     })
   },
 );
+
+//npx --ignore-script=false inngest-cli@latest dev
