306712f6cd6d254a2ee3d3186f23e04931d1779f
Vedang Mishrra <145650247+justvedang@users.noreply.github.com>
2026-03-04 01:12:22 +0530

    completed project page setup

--- src/features/projects/components/projects-view.tsx
+++ src/features/projects/components/projects-view.tsx
@@ -1,19 +1,46 @@
 "use client"
 
 import { Poppins } from "next/font/google"
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { SparkleIcon } from "lucide-react"
 import { Kbd } from "@/components/ui/kbd"
 import {FaGithub} from "react-icons/fa"
+import { ProjectsList } from "./projects-list"
+import { useCreateProject } from "../hooks/use-projects"
+import {adjectives,animals,colors,uniqueNamesGenerator} from "unique-names-generator"
+import { useEffect,useState } from "react"
+import { ProjectsCommandDialog } from "./projects-command-dialog"
 
 const font=Poppins({
     subsets:["latin"],
     weight:["400","500","600","700"]
 })
+
+
 export const ProjectsView=()=>{
+    const createProject=useCreateProject();
+    const [commandDialogOpen,setCommandDialogOpen]=useState(false);
+    useEffect(()=>{
+        const handleKeyDown=(e:KeyboardEvent)=>{
+            if(e.metaKey || e.ctrlKey){
+                if(e.key==="k"){
+                    e.preventDefault();
+                    setCommandDialogOpen(true);
+
+                }
+            }
+        }
+
+        document.addEventListener("keydown",handleKeyDown);
+        return ()=> document.removeEventListener("keydown",handleKeyDown)
+    },[])
     return (
+        <>
+        <ProjectsCommandDialog 
+        open={commandDialogOpen}
+        onOpenChange={setCommandDialogOpen}
+        />
         <div className="min-h-screen bg-sidebar flex flex-col
          items-center justify-center p-6 md:p-16">
             <div className="w-full max-w-sm mx-auto flex flex-col gap-4 items-center">
@@ -21,7 +48,7 @@ export const ProjectsView=()=>{
                 <div className="flex justify-between gap-4 w-full items-center">
 
                     <div className="flex items-center gap-2 w-full group/logo">
-                        <img src="/vercel.svg" alt="Maverick" className="size-[32px] md:size-[46px]" />
+                        <img src="/logo.svg" alt="Maverick" className="size-[32px] md:size-[46px]" />
 
                     
                     <h1 className={cn(
@@ -36,7 +63,16 @@ export const ProjectsView=()=>{
                 <div className="flex flex-col gap-4 w-full">
                     <div className="grid grid-cols-2 gap-2 ">
                         <Button variant="outline" 
-                        onClick={()=>{}}
+                        onClick={()=>{
+                            const projectName=uniqueNamesGenerator({
+                                dictionaries:[adjectives,animals,colors],
+                                separator:"-",
+                                length:3,
+                            })
+                            createProject({
+                                name:projectName,
+                            });
+                        }}
                         className="h-full items-start justify-start p-4 bg-background border flex flex-col gap-6 rounded-none"
                         >
                             <div className="flex items-center justify-between w-full">
@@ -70,12 +106,14 @@ export const ProjectsView=()=>{
 
                     </div>
 
-                   
+                   <ProjectsList onViewAll={()=>{setCommandDialogOpen(true)}}/>
 
                 </div>
 
             </div>
 
         </div>
+        
+        </>
     )
 }
